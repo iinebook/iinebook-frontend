34f56f0e308364f4ce9620261d979fdf5f2aaa28
keinahc <i.k.6oranges@gmail.com>
2018-02-28 08:50:13 +0900

    Formatted code with WebStorm

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,7 +35,6 @@
           Twitter
         </a>
       </li>
-      <br>
       <li>
         <a
           href="http://vuejs-templates.github.io/webpack/"
@@ -84,30 +83,33 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
